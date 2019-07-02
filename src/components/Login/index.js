215088ea892cde302243ae713940010b96144c93
Reiner <reiner.ddview@gmail.com>
2019-07-03 01:39:06 +0200

    First attempt at connecting sockets and building the systems depending on it.

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -4,17 +4,41 @@ import "./style.css";
 
 export default function Login({ setLogin }) {
   const loginStatus = useContext(Context);
-  return (
-    <label className="switch">
-      <input
-        type="checkbox"
-        checked={loginStatus}
-        onChange={({ target }) => {
-          setLogin(target.checked);
-        }}
-      />
-      <span className="slider round" />
-    </label>
-  );
-  // return <button onClick={() => setLogin(true)}>LOGIN</button>;
+
+  const loginFunction = () => {
+    const cachedPassword = localStorage.getItem("pass");
+    localStorage.setItem(
+      "pass",
+      prompt("Password:", cachedPassword ? cachedPassword : "")
+    );
+    //TODO: make request with password , get authkey
+    const authkey = "abc";
+    localStorage.setItem("authkey", authkey);
+    setLogin({ ...loginStatus, authkey: authkey });
+  };
+
+  const logoutFunction = () => {
+    localStorage.removeItem("authkey");
+    setLogin({ ...loginStatus, authkey: null });
+  };
+
+  if (loginStatus.authkey) {
+    return (
+      <div className="Login">
+        <button onClick={logoutFunction}>Logout</button>
+        <label className="switch">
+          <input
+            type="checkbox"
+            checked={loginStatus.visible}
+            onChange={({ target }) => {
+              setLogin({ ...loginStatus, visible: target.checked });
+            }}
+          />
+          <span className="slider round" />
+        </label>
+      </div>
+    );
+  } else {
+    return <button onClick={loginFunction}>Login</button>;
+  }
 }
--- src/components/Playlist/index.js
+++ src/components/Playlist/index.js
@@ -1,6 +1,8 @@
-import React, { useEffect, useReducer } from "react";
+import React, { useEffect, useReducer, useContext } from "react";
 import axios from "axios";
 import Song from "../Song/";
+import { Context as loginContext } from "../../Contexts/loginStatus";
+import { Context as socketContext } from "../../Contexts/socketIO";
 import "./style.css";
 
 function playlistReducer(state, action) {
@@ -17,29 +19,57 @@ function playlistReducer(state, action) {
   }
 }
 
+//TODO: Switch songs on server to some kind of {id, name} object.
 export default function Playlist() {
   const [state, dispatch] = useReducer(playlistReducer, []);
+  const loginStatus = useContext(loginContext);
+  const socket = useContext(socketContext);
 
+  //TODO: Switch to loading the playlist only on signal from the socket and on initial load.
+  //possible:
+  // socket.on('playlist', loadPlaylist);
+  // and removal of the interval further down
+  //other possibility, socket sends the playlist with the message:
+  // socket.on("playlist", data => {
+  //   dispatch({ type: "SET", payload: data.reverse() });
+  // });
+  // Initial load still with axios, afterwards on signal, maybe send a 'playlist' event to server on connect
   async function loadPlaylist() {
     try {
       const res = await axios.get("/playlist");
       if (res.status === 200)
-        dispatch({ type: "SET", payload: res.data.reverse() });
+        dispatch({
+          type: "SET",
+          payload: res.data.reverse().map(song => {
+            return {
+              id: song.id ? song.id : song,
+              name: song.name ? song.name : song
+            };
+          })
+        });
     } catch (err) {
       //Do nothing
     }
   }
 
   async function skipSong(id) {
-    // const res = await axios.post("/auth/skipSong/", id);
-    // if(res.status === 200)
+    //TODO:
+    // socket.emit("skip", { id, authkey: loginStatus.authkey }, (response) => {
+    //   if(response.status === 'done')
+    //   dispatch({ type: "REMOVE", payload: id });
+    //   alert("skipped Song: " + id);
+    // });
     dispatch({ type: "REMOVE", payload: id });
     alert("skipped Song: " + id);
   }
 
   async function removeSong(id) {
-    // const res = await axios.post("/auth/removeSong/", id);
-    // if(res.status === 200)
+    //TODO:
+    // socket.emit("remove", { id, authkey: loginStatus.authkey }, (response) => {
+    //   if(response.status === 'done')
+    //   dispatch({ type: "REMOVE", payload: id });
+    //   alert("deleted Song: " + id);
+    // });
     dispatch({ type: "REMOVE", payload: id });
     alert("deleted Song: " + id);
   }
