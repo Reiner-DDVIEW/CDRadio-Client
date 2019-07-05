d70a601b44e46cb725a57ba66da9b0d2c90670a2
Reiner <reiner.ddview@gmail.com>
2019-07-05 22:26:26 +0200

    Reducing re-renders when possible. By using memo techniques the Uploader and the Songs no longer re-calculate everything on every render.

--- src/components/Song/index.js
+++ src/components/Song/index.js
@@ -1,22 +1,23 @@
-import React, { useContext } from "react";
+import React, { useContext, useCallback } from "react";
 import { Context } from "../../Contexts/loginStatus";
 import "./style.css";
 
-export default function Song({ song, remove, skip }) {
-  const { id, name } = song;
+export default React.memo(function Song({ name, id, remove, skip }) {
   const loginStatus = useContext(Context);
+  const onRemove = useCallback(() => remove(id), [id, remove]);
+  const onSkip = useCallback(() => skip(id), [id, skip]);
   return (
     <div className="Song">
       <li>
         <p>{name}</p>
         {loginStatus.authkey && loginStatus.visible && (
           <>
-            <button onClick={() => skip(id)}>>></button>
-            <button onClick={() => remove(id)}>X</button>
+            <button onClick={onSkip}>>></button>
+            <button onClick={onRemove}>X</button>
           </>
         )}
       </li>
       <hr />
     </div>
   );
-}
+});
