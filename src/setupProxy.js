e120f3e295a7782c59da6a1427ad51da0c70d8d2
Reiner <reiner.ddview@gmail.com>
2019-07-08 19:54:35 +0200

    Proxy to the radio on a different port.

---
+++ src/setupProxy.js
@@ -0,0 +1,9 @@
+const proxy = require("http-proxy-middleware");
+
+module.exports = function(app) {
+  app.use(proxy("/playlist", { target: "http://localhost:5000/" }));
+  app.use(proxy("/allowed", { target: "http://localhost:5000/" }));
+  app.use(proxy("/upload", { target: "http://localhost:5000/" }));
+  app.use(proxy("/youtube", { target: "http://localhost:5000/" }));
+  app.use(proxy("/socket.io", { target: "http://localhost:5000/", ws: true }));
+};
