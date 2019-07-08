31d4f66f1c0c59a6edb4269c18a5dc0ef382e95c
Reiner <reiner.ddview@gmail.com>
2019-07-08 21:57:41 +0200

    Limiting songs to one row, displaying the time on its own.

--- src/components/Song/index.js
+++ src/components/Song/index.js
@@ -1,15 +1,23 @@
-import React, { useContext, useCallback } from "react";
+import React, { useContext, useCallback, useMemo } from "react";
 import { Context } from "../../Contexts/loginStatus";
 import "./style.css";
+
+//TODO: Remove after the server sends an object with id,time, and name for the songs
+function extractTime(name) {
+  const index = name.lastIndexOf("(");
+  return { displayname: name.slice(0, index), time: name.slice(index) };
+}
 
 export default React.memo(function Song({ name, id, remove, skip }) {
   const loginStatus = useContext(Context);
   const onRemove = useCallback(() => remove(id), [id, remove]);
   const onSkip = useCallback(() => skip(id), [id, skip]);
+  const { displayname, time } = useMemo(() => extractTime(name), [name]);
   return (
     <div className="Song">
       <li>
-        <p>{name}</p>
+        <p title={displayname}>{displayname}</p>
+        <span>{time}</span>
         {loginStatus.authkey && loginStatus.visible && (
           <>
             <button onClick={onSkip}>>></button>
